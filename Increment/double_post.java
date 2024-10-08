3712a966c45ce62a62c7fe60f4fcfba54d25c2ea
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-10-08 20:53:48 +0530

    Add files via upload

---
+++ Increment/double_post.java
@@ -0,0 +1,11 @@
+public class double_post {
+    public static void main(String args[]){
+int a=5, b;
+
+b=a++;
+System.out.println(b);
+
+
+
+    }
+}
---
+++ Increment/two2_post.java
@@ -0,0 +1,10 @@
+public class two2_post {
+        public static void main (String args[]){
+           int a=5 ,b=3 ,c=4, x; 
+    
+           x=a*b+++3*c++;       /*both are post increment*/
+           System.out.println(x);
+        }
+    }
+    
+
