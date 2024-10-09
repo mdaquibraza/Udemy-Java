cd6acc2ae478f6e57ab501d08bdb2139d3e013db
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-10-09 15:13:58 +0530

    Add files via upload

---
+++ bitwise_operator.java
@@ -0,0 +1,22 @@
+public class bitwise_operator{
+    public static void main(String args[]){
+
+        int a=10 ,b=6 ,z;
+
+        z=a&b;
+        System.out.println("and operator "+z);
+
+
+
+
+z=a|b;
+System.out.println("or operator "+z);
+
+
+z=a^b;
+System.out.println("xor operator "+z);
+
+    }
+
+
+}
