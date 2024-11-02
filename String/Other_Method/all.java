2ac728d0a6f732b51f3021b47ab7b5bc0265c58e
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:52:47 +0530

    Add files via upload

---
+++ String/Other_Method/all.java
@@ -0,0 +1,40 @@
+class all{
+public static void main(String[] args) {
+    
+String str="aquib raza";
+
+
+int length=str.length();
+System.out.println(length);
+
+
+
+boolean start=str.startsWith("a");
+System.out.println(start);
+
+
+boolean start1=str.startsWith("z");
+System.out.println(start1);
+
+
+
+
+boolean end=str.startsWith("a");
+System.out.println(end);
+
+
+boolean end1=str.startsWith("b");
+System.out.println(end1);
+
+
+
+char s=str.charAt(4);
+System.out.println(s);
+
+
+
+}
+
+
+
+}
