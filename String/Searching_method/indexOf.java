a806c27fa5f8ef4c732a3d7a401db22f3c619f6d
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:49:48 +0530

    Add files via upload

---
+++ String/Searching_method/indexOf.java
@@ -0,0 +1,23 @@
+public class indexOf{
+public static void main(String[] args) {
+    
+String  str="aquib raza";
+
+int indexof=str.indexOf("u");
+System.out.println( " index of  "+indexof);
+
+
+int indexof1=str.lastIndexOf("");
+System.out.println( "last index of  "+indexof1);
+
+
+int indexof2=str.lastIndexOf("raza");
+System.out.println("last index of 2  "+indexof2);
+
+
+
+}
+
+
+
+}
