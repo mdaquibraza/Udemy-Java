b19e175c0fdce3617965d2b6e623d9179a3429ea
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:51:42 +0530

    Add files via upload

---
+++ String/Trim/trim.java
@@ -0,0 +1,18 @@
+class trim{
+
+
+    public static void main(String args[]){
+
+String str="     aquib raza    ";
+System.out.println(str);
+
+
+
+String trim=str.trim();
+System.out.println(trim);
+
+
+String strip=str.strip();
+System.out.println(strip);
+    }
+}
