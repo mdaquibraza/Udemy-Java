584c0f75ca971c92806888cd53d46ab84a32ac14
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:44:42 +0530

    Add files via upload

---
+++ String/Case_Conversation_Method/lower.java
@@ -0,0 +1,11 @@
+class lower{
+
+    public static void main(String args[]){
+
+
+        String str="Aquib Raza";
+
+        String lower=str.toLowerCase();
+        System.out.println(lower);
+    }
+}
---
+++ String/Case_Conversation_Method/upeer.java
@@ -0,0 +1,12 @@
+public class upeer {
+    
+    public static void main(String args[]){
+
+
+        String str="Aquib Raza";
+
+        String upper=str.toUpperCase();
+        System.out.println(upper);
+    }
+}
+
