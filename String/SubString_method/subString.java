0ecd87a62d963c8e8a6ce6ab94de8b70986e1497
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:50:54 +0530

    Add files via upload

---
+++ String/SubString_method/subString.java
@@ -0,0 +1,22 @@
+public class subString{
+public static void main(String[] args) {
+    
+
+    String str="aquib raza";
+
+
+
+ String  sub=str.substring(4);
+System.out.println(sub);
+
+
+String  sub2=str.substring(4,9);
+System.out.println(sub2);
+
+
+
+
+
+}
+}
+
