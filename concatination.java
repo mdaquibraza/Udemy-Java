f4b84dc621449c6d9bbb76d974cc4e007210bac4
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:37:45 +0530

    Add files via upload

---
+++ concatination.java
@@ -0,0 +1,14 @@
+class concatination{
+    public static void main(String[] args) {
+        
+        int a=43 , b=56;   
+
+        System.out.println("sum of "+a+" and "+b+" is "+(a+b));
+
+
+      float c=32.6f , d=22.5f;
+      System.out.println(".sum of "+c+" and "+d+" is "+(c+d));
+
+
+    }
+}
