32251fe8451193d5866e0accadf9f70f6c88a0b2
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-02 22:48:50 +0530

    Add files via upload

---
+++ String/Replacement_method/replace.java
@@ -0,0 +1,21 @@
+class replace{
+public static void main(String args[]){
+
+
+
+    String str="aquib raza";
+
+    String replace=str.replace('a','f');
+    System.out.println(replace);
+
+    String replace2=str.replace('a','n');
+    System.out.println(replace2);
+
+
+
+
+
+}
+}
+
+
