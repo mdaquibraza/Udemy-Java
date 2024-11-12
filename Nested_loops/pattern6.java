d4b56c20f43717f546095d05b94b5875ef1b2b1d
Md Aquib Raza <157198381+mdaquibraza@users.noreply.github.com>
2024-11-12 20:51:01 +0530

    Add files via upload

---
+++ Nested_loops/pattern6.java
@@ -0,0 +1,21 @@
+public class pattern6 {
+    
+
+    public static void main(String[] args) {
+        
+
+int add=0;
+
+        for(int i=1; i<=5;i++){
+
+
+            for(int j=1; j<=i; j++){
+
+
+                add++;
+                System.out.print(add+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
